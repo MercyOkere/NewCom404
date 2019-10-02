8bff3cc83dad94d3fc64bb4a520b26cd4e0fcb34
MercyOkere <3okerm29@solent.ac.uk>
2019-10-02 11:31:39 +0000

    completed task

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,10 @@
+# Ask user for eye character
+print("Please enter eye characater")
+eye = input()
+
+
+# Display an ascii art robot
+print("##########")
+print("#   " + eye + "  " + eye + "  #")
+print("# ---- #")
+print("##########")
